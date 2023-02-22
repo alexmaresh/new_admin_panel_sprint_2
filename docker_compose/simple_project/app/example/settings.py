f6da755fabe949b518e689f14cea7805f10e5428
Alexandra Maresh <maresh.al@yandex.ru>
2023-02-22 23:56:11 +0300

    movieslistapi

--- docker_compose/simple_project/app/example/settings.py
+++ docker_compose/simple_project/app/example/settings.py
@@ -63,24 +63,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'example.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('DB_PORT', 5432),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
         'OPTIONS': {
             'options': '-c search_path=public,content'
         }
@@ -128,3 +118,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+ADMINS_ROWS_PER_PAGE = 20
+API_MOVIES_PER_PAGE = 50
--- docker_compose/simple_project/app/movies/api/v1/urls.py
+++ docker_compose/simple_project/app/movies/api/v1/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from movies.api.v1 import views
 
+
 urlpatterns = [
-    path('movies/', views.MoviesListApi.as_view())
+    path('movies/', views.Movies.as_view(), name='Movies'),
+    path('movies/<uuid:pk>/', views.MoviesDetailApi.as_view(), name='MoviesDetail')
 ]
