7565e2f68a01b0038c68f0a0aceffd18457b1b24
Alexandra Maresh <maresh.al@yandex.ru>
2023-02-23 21:27:36 +0300

    fix review1: server tokens off, add admin models

--- docker_compose/simple_project/app/example/settings.py
+++ docker_compose/simple_project/app/example/settings.py
@@ -11,14 +11,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 # Application definition
 
--- docker_compose/simple_project/app/movies/admin.py
+++ docker_compose/simple_project/app/movies/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Genre, GenreFilmwork
+from .models import Filmwork
+from .models import Person, PersonFilmwork
 
-# Register your models here.
+
+class GenreFilmworkInline(admin.TabularInline):
+    model = GenreFilmwork
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created', 'modified')
+    list_filter = ('name',)
+    search_fields = ('name', 'id')
+
+
+@admin.register(Filmwork)
+class FilmworkAdmin(admin.ModelAdmin):
+    inlines = (GenreFilmworkInline,)
+    list_display = ('title', 'type', 'creation_date', 'rating', 'created', 'modified')
+    list_filter = ('type',)
+    search_fields = ('title', 'description', 'id')
+
+
+class PersonFilmworkInline(admin.TabularInline):
+    model = PersonFilmwork
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    inlines = (PersonFilmworkInline,)
+    list_display = ('full_name', 'created', 'modified')
+    list_filter = ('full_name',)
+    search_fields = ('full_name',)
