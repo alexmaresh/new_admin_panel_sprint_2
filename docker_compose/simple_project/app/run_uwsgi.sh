257f5aad4d51e845b8e08f77e032b3875cb75c84
Alexandra Maresh <maresh.al@yandex.ru>
2023-02-23 22:33:46 +0300

    fix review1: migrations, superuser,collectstatic

--- docker_compose/simple_project/app/run_uwsgi.sh
+++ docker_compose/simple_project/app/run_uwsgi.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-set -e
+export DJANGO_SUPERUSER_USERNAME=testname
+export DJANGO_SUPERUSER_EMAIL=test@test.com
+export DJANGO_SUPERUSER_PASSWORD=testpass1234
+python manage.py createsuperuser --no-input || true
 
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
+
+python manage.py compilemessages -l en -l ru
+
+set -e
 chown www-data:www-data /var/log
 
 uwsgi --strict --ini /etc/app/uwsgi.ini
