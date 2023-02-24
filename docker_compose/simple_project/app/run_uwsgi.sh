586bab52bca8a8707d87cadd44b401a129cfc9d0
Alexandra Maresh <maresh.al@yandex.ru>
2023-02-24 12:13:16 +0300

    fix review2: fix run script

--- docker_compose/simple_project/app/run_uwsgi.sh
+++ docker_compose/simple_project/app/run_uwsgi.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-export DJANGO_SUPERUSER_USERNAME=testname
-export DJANGO_SUPERUSER_EMAIL=test@test.com
-export DJANGO_SUPERUSER_PASSWORD=testpass1234
-python manage.py createsuperuser --no-input || true
-
-python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
+python3 manage.py createsuperuser --no-input || true
 python3 manage.py collectstatic --noinput
 
 python manage.py compilemessages -l en -l ru
